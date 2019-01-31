0f8c9cb387dadc4f73cb52030a5dff8fdbc0c99a
rauljgb <sh.raul.jgb@gmail.com>
2019-01-31 11:07:22 -0500

    actualizacion ticket y nota PDF

--- app/CoreFacturalo/Templates/pdf/invoice_ticket.blade.php
+++ app/CoreFacturalo/Templates/pdf/invoice_ticket.blade.php
@@ -3,111 +3,100 @@
     $customer = $document->customer;
     $invoice = $document->invoice;
     $path_style = app_path('CoreFacturalo'.DIRECTORY_SEPARATOR.'Templates'.DIRECTORY_SEPARATOR.'pdf'.DIRECTORY_SEPARATOR.'style.css');
+    $document_number = $document->series.'-'.str_pad($document->number, 8, '0', STR_PAD_LEFT);
 @endphp
 <html>
 <head>
-    <title>{{ $document->number_full }}</title>
+    <title>{{ $document_number }}</title>
     <link href="{{ $path_style }}" rel="stylesheet" />
 </head>
 <body>
 <table class="full-width">
     <tr>
         @if($company->logo)
-            <td width="25%">
+            <td class="text-center px-5 pt-5 pb-3">
                 <img src="{{ asset('storage/uploads/logos/'.$company->logo) }}" class="company_logo">
             </td>
         @endif
-        <td width="100%">
-            <table class="">
-                <tbody>
-                <tr><td class="text-left font-xxlg font-bold">{{ $company->name }}</td></tr>
-                <tr><td class="text-left font-xl font-bold">{{ 'RUC '.$company->number }}</td></tr>
-                <tr><td class="text-left font-lg">{{ $establishment->address }}</td></tr>
-                <tr><td class="text-left font-lg">{{ ($establishment->email !== '-')? $establishment->email : '' }}</td></tr>
-                <tr><td class="text-left font-lg font-bold">{{ ($establishment->telephone !== '-')? $establishment->telephone : '' }}</td></tr>
-                </tbody>
-            </table>
-        </td>
-        <td width="30%">
-            <table class="border-box">
-                <tbody>
-                <tr><td class="text-center font-lg">{{ $document->document_type->description }}</td></tr>
-                <tr><td class="text-center font-xlg font-bold">{{ $document->number_full }}</td></tr>
-                </tbody>
-            </table>
-        </td>
+    </tr>
+    <tr>
+        <td class="text-center"><h3>{{ $company->name }}</h3></td>
+    </tr>
+    <tr>
+        <td class="text-center"><h4>{{ 'RUC '.$company->number }}</h4></td>
+    </tr>
+    <tr>
+        <td class="text-center">{{ ($establishment->address !== '-')? $establishment->address : '' }}</td>
+    </tr>
+    <tr>
+        <td class="text-center">{{ ($establishment->email !== '-')? $establishment->email : '' }}</td>
+    </tr>
+    <tr>
+        <td class="text-center pb-3">{{ ($establishment->telephone !== '-')? $establishment->telephone : '' }}</td>
+    </tr>
+    <tr>
+        <td class="text-center pt-3 border-top"><h4>{{ $document->document_type->description }}</h4></td>
+    </tr>
+    <tr>
+        <td class="text-center pb-3 border-bottom"><h3>{{ $document_number }}</h3></td>
     </tr>
 </table>
 <table class="full-width">
     <tr>
-        <td width="55%" class="align-top">
-            <table class="">
-                <tbody>
-                <tr>
-                    <td width="50%">Fecha de emisión: </td>
-                    <td width="50%">{{ $document->date_of_issue->format('Y-m-d') }}</td>
-                </tr>
-                @if($invoice->date_of_due)
-                    <tr>
-                        <td width="50%">Fecha de vencimiento: </td>
-                        <td width="50%">{{ $invoice->date_of_due->format('Y-m-d') }}</td>
-                    </tr>
-                @endif
-                <tr>
-                    <td width="20%">Cliente:</td>
-                    <td width="80%">{{ $customer->name }}</td>
-                </tr>
-                <tr>
-                    <td width="20%">{{ $customer->identity_document_type->description }}:</td>
-                    <td width="80%">{{ $customer->number }}</td>
-                </tr>
-                @if ($customer->address !== '')
-                    <tr>
-                        <td width="20%">Dirección:</td>
-                        <td width="80%">{{ $customer->address }}</td>
-                    </tr>
-                @endif
-                </tbody>
-            </table>
-        </td>
-        <td width="45%" class="align-top">
-            <table class="">
-                <tbody>
-                @if ($document->purchase_order)
-                    <tr>
-                        <td width="50%">Orden de Compra: </td>
-                        <td width="50%">{{ $document->purchase_order }}</td>
-                    </tr>
-                @endif
-                @if ($document->guides)
-                    @foreach($document->guides as $guide)
-                        <tr>
-                            <td>{{ $guide->document_type_id }}</td>
-                            <td>{{ $guide->number }}</td>
-                        </tr>
-                    @endforeach
-                @endif
-                </tbody>
-            </table>
-        </td>
+        <td width="45%" class="pt-3"><p class="desc">Fecha de emisión:</p></td>
+        <td width="" class="pt-3"><p class="desc">{{ $document->date_of_issue->format('Y-m-d') }}</p></td>
     </tr>
+    @if($invoice->date_of_due)
+    <tr>
+        <td><p class="desc">Fecha de vencimiento:</p></td>
+        <td><p class="desc">{{ $invoice->date_of_due->format('Y-m-d') }}</p></td>
+    </tr>
+    @endif
+    <tr>
+        <td><p class="desc">Cliente:</p></td>
+        <td><p class="desc">{{ $customer->name }}</p></td>
+    </tr>
+    <tr>
+        <td><p class="desc">{{ $customer->identity_document_type->description }}:</p></td>
+        <td><p class="desc">{{ $customer->number }}</p></td>
+    </tr>
+    @if ($customer->address !== '')
+        <tr>
+            <td class="align-top"><p class="desc">Dirección:</p></td>
+            <td><p class="desc">{{ $customer->address }}</p></td>
+        </tr>
+    @endif
+    @if ($document->purchase_order)
+        <tr>
+            <td><p class="desc">Orden de Compra:</p></td>
+            <td><p class="desc">{{ $document->purchase_order }}</p></td>
+        </tr>
+    @endif
+    @if ($document->guides)
+        @foreach($document->guides as $guide)
+            <tr>
+                <td><p class="desc">{{ $guide->document_type_id }}</p></td>
+                <td><p class="desc">{{ $guide->number }}</p></td>
+            </tr>
+        @endforeach
+    @endif
 </table>
 <table class="full-width mt-10 mb-10">
     <thead class="">
     <tr>
-        <th class="border-top-bottom text-center">CANT.</th>
-        <th class="border-top-bottom text-center">UNIDAD</th>
-        <th class="border-top-bottom text-left">DESCRIPCIÓN</th>
-        <th class="border-top-bottom text-right">P.UNIT</th>
-        <th class="border-top-bottom text-right">TOTAL</th>
+        <th class="border-top-bottom desc">CANT.</th>
+        <th class="border-top-bottom desc">UNIDAD</th>
+        <th class="border-top-bottom desc">DESCRIPCIÓN</th>
+        <th class="border-top-bottom desc">P.UNIT</th>
+        <th class="border-top-bottom desc">TOTAL</th>
     </tr>
     </thead>
     <tbody>
     @foreach($document->items as $row)
         <tr>
-            <td class="text-center">{{ $row->quantity }}</td>
-            <td class="text-center">{{ $row->item->unit_type_id }}</td>
-            <td class="text-center">
+            <td class="text-center desc">{{ $row->quantity }}</td>
+            <td class="text-center desc">{{ $row->item->unit_type_id }}</td>
+            <td class="text-center desc">
                 {!! $row->item->description !!}
                 @if($row->attributes)
                     @foreach($row->attributes as $attr)
@@ -115,75 +104,64 @@
                     @endforeach
                 @endif
             </td>
-            <td class="text-right">{{ number_format($row->unit_price, 2) }}</td>
-            <td class="text-right">{{ number_format($row->total, 2) }}</td>
+            <td class="text-right desc">{{ number_format($row->unit_price, 2) }}</td>
+            <td class="text-right desc">{{ number_format($row->total, 2) }}</td>
+        </tr>
+        <tr>
+            <td colspan="5" class="border-bottom"></td>
         </tr>
     @endforeach
+        @if($document->total_exportation > 0)
+            <tr>
+                <td colspan="4" class="text-right font-bold desc">OP. EXPORTACIÓN: {{ $document->currency_type->symbol }}</td>
+                <td class="text-right font-bold desc">{{ number_format($document->total_exportation, 2) }}</td>
+            </tr>
+        @endif
+        @if($document->total_free > 0)
+            <tr>
+                <td colspan="4" class="text-right font-bold desc">OP. GRATUITAS: {{ $document->currency_type->symbol }}</td>
+                <td class="text-right font-bold desc">{{ number_format($document->total_free, 2) }}</td>
+            </tr>
+        @endif
+        @if($document->total_unaffected > 0)
+            <tr>
+                <td colspan="4" class="text-right font-bold desc">OP. INAFECTAS: {{ $document->currency_type->symbol }}</td>
+                <td class="text-right font-bold desc">{{ number_format($document->total_unaffected, 2) }}</td>
+            </tr>
+        @endif
+        @if($document->total_exonerated > 0)
+            <tr>
+                <td colspan="4" class="text-right font-bold desc">OP. EXONERADAS: {{ $document->currency_type->symbol }}</td>
+                <td class="text-right font-bold desc">{{ number_format($document->total_exonerated, 2) }}</td>
+            </tr>
+        @endif
+        @if($document->total_taxed > 0)
+            <tr>
+                <td colspan="4" class="text-right font-bold desc">OP. GRAVADAS: {{ $document->currency_type->symbol }}</td>
+                <td class="text-right font-bold desc">{{ number_format($document->total_taxed, 2) }}</td>
+            </tr>
+        @endif
+        <tr>
+            <td colspan="4" class="text-right font-bold desc">IGV: {{ $document->currency_type->symbol }}</td>
+            <td class="text-right font-bold desc">{{ number_format($document->total_igv, 2) }}</td>
+        </tr>
+        <tr>
+            <td colspan="4" class="text-right font-bold desc">TOTAL A PAGAR: {{ $document->currency_type->symbol }}</td>
+            <td class="text-right font-bold desc">{{ number_format($document->total, 2) }}</td>
+        </tr>
     </tbody>
 </table>
 <table class="full-width">
     <tr>
-        <td width="35%">
-            <table>
-                <tbody>
-                <tr>
-                    <td class="text-center"><img class="qr_code" src="data:image/png;base64, {{ $document->qr }}" /></td>
-                </tr>
-                <tr>
-                    <td>Código Hash: {{ $document->hash }}</td>
-                </tr>
-                @foreach($document->legends as $row)
-                    <tr>
-                        <td class="font-bold">{{ $row->value }} {{ $document->currency_type->description }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </td>
-        <td width="65%">
-            <table class="">
-                <tbody>
-                @if($document->total_exportation > 0)
-                    <tr>
-                        <td class="text-right font-lg font-bold" width="70%">OP. EXPORTACIÓN: {{ $document->currency_type->symbol }}</td>
-                        <td class="text-right font-lg font-bold" width="30%">{{ number_format($document->total_exportation, 2) }}</td>
-                    </tr>
-                @endif
-                @if($document->total_free > 0)
-                    <tr>
-                        <td class="text-right font-lg font-bold" width="70%">OP. GRATUITAS: {{ $document->currency_type->symbol }}</td>
-                        <td class="text-right font-lg font-bold" width="30%">{{ number_format($document->total_free, 2) }}</td>
-                    </tr>
-                @endif
-                @if($document->total_unaffected > 0)
-                    <tr>
-                        <td class="text-right font-lg font-bold" width="70%">OP. INAFECTAS: {{ $document->currency_type->symbol }}</td>
-                        <td class="text-right font-lg font-bold" width="30%">{{ number_format($document->total_unaffected, 2) }}</td>
-                    </tr>
-                @endif
-                @if($document->total_exonerated > 0)
-                    <tr>
-                        <td class="text-right font-lg font-bold" width="70%">OP. EXONERADAS: {{ $document->currency_type->symbol }}</td>
-                        <td class="text-right font-lg font-bold" width="30%">{{ number_format($document->total_exonerated, 2) }}</td>
-                    </tr>
-                @endif
-                @if($document->total_taxed > 0)
-                    <tr>
-                        <td class="text-right font-lg font-bold" width="70%">OP. GRAVADAS: {{ $document->currency_type->symbol }}</td>
-                        <td class="text-right font-lg font-bold" width="30%">{{ number_format($document->total_taxed, 2) }}</td>
-                    </tr>
-                @endif
-                <tr>
-                    <td class="text-right font-lg font-bold" width="70%">IGV: {{ $document->currency_type->symbol }}</td>
-                    <td class="text-right font-lg font-bold" width="30%">{{ number_format($document->total_igv, 2) }}</td>
-                </tr>
-                <tr>
-                    <td class="text-right font-lg font-bold" width="70%">TOTAL A PAGAR: {{ $document->currency_type->symbol }}</td>
-                    <td class="text-right font-lg font-bold" width="30%">{{ number_format($document->total, 2) }}</td>
-                </tr>
-                </tbody>
-            </table>
-        </td>
+        @foreach($document->legends as $row)
+            <td colspan="5" class="desc pt-3">Son: <span class="font-bold">{{ $row->value }} {{ $document->currency_type->description }}</span></td>
+        @endforeach
+    </tr>
+    <tr>
+        <td class="text-center pt-5"><img class="qr_code" src="data:image/png;base64, {{ $document->qr }}" /></td>
+    </tr>
+    <tr>
+        <td class="text-center desc">Código Hash: {{ $document->hash }}</td>
     </tr>
 </table>
 </body>
