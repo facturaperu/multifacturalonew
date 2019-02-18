368286789a6d3e03835a6fb4b296595068393d19
PERU CPE <perucpe@gmail.com>
2019-02-18 04:25:59 -0500

    recarga de logo en modulo documents, refiniendo los modulos

--- database/migrations/tenant/2018_01_01_000004_tenant_modules_table.php
+++ database/migrations/tenant/2018_01_01_000004_tenant_modules_table.php
@@ -23,19 +23,10 @@ class TenantModulesTable extends Migration
 
         DB::table('modules')->insert([
             ['value' => 'documents', 'description' => 'Ventas'],
-            ['value' => 'items', 'description' => 'Productos'],
-            ['value' => 'customers', 'description' => 'Clientes'],
-            ['value' => 'suppliers', 'description' => 'Proveedores'],
             ['value' => 'purchases', 'description' => 'Compras'],
-            ['value' => 'summaries', 'description' => 'Resúmenes'],
-            ['value' => 'voided', 'description' => 'Anulaciones'],
-            ['value' => 'companies', 'description' => 'Empresa'],
-            ['value' => 'retentions', 'description' => 'Retenciones'],
-            ['value' => 'dispatches', 'description' => 'Guías de remisión'],
-            ['value' => 'users', 'description' => 'Usuarios'],
-            ['value' => 'establishments', 'description' => 'Establecimientos'],
-            ['value' => 'catalogs', 'description' => 'Catálogos'],
-            ['value' => 'advanced', 'description' => 'Avanzado'],
+            ['value' => 'advanced', 'description' => 'Documentos Avanzados'],
+            ['value' => 'reports', 'description' => 'Reportes'],
+            ['value' => 'configuration', 'description' => 'Configuration'],
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ if ($hostname) {
         Route::get('downloads/{model}/{type}/{external_id}/{format?}', 'Tenant\DownloadController@downloadExternal')->name('tenant.download.external_id');
         Route::get('print/{model}/{external_id}/{format?}', 'Tenant\DownloadController@toPrint');
 
-        Route::middleware(['auth', 'module'])->group(function() {
+//        Route::middleware(['auth', 'module'])->group(function() {
+        Route::middleware(['auth'])->group(function() {
             Route::get('/', function () {
                 return redirect()->route('tenant.documents.create');
             });
