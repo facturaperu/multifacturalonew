d002acf19e59f5ba2c9e7fc436b1aea37822b38e
Brayan Mendoza Arango <bemendozaa@gmail.com>
2019-03-01 11:57:03 -0500

    ajustes campos - reportes

--- resources/views/tenant/reports/kardex/index.blade.php
+++ resources/views/tenant/reports/kardex/index.blade.php
@@ -64,13 +64,13 @@
                                         <td>{{$value->created_at}}</td>
                                         <td>{{($value->type == 'sale') ? 'Venta' : 'Compra'}}</td>
                                         <td>{{($value->type == 'sale') ? "{$value->document->series}-{$value->document->number}" : "{$value->purchase->series}-{$value->purchase->number}"}}</td>
-                                        <td>{{($value->type == 'purchase') ? number_format($value->quantity, 2) : number_format(0, 2)}}</td>
-                                        <td>{{($value->type == 'sale') ? number_format($value->quantity, 2) : number_format(0, 2)}}</td>
+                                        <td>{{($value->type == 'purchase') ? number_format($value->quantity, 4) : number_format(0, 4)}}</td>
+                                        <td>{{($value->type == 'sale') ? number_format($value->quantity, 4) : number_format(0, 4)}}</td>
                                         @php
                                             if ($value->type == 'purchase') $balance += $value->quantity;
                                             if ($value->type == 'sale') $balance -= $value->quantity;
                                         @endphp
-                                        <td>{{number_format($balance, 2)}}</td>
+                                        <td>{{number_format($balance, 4)}}</td>
                                     </tr>
                                     @endforeach
                                 </tbody>
