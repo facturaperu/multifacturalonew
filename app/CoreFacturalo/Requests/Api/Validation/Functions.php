1ba97fa9a38bb9cc6642be99ee5cb5de2d118304
rauljgb <sh.raul.jgb@gmail.com>
2019-04-05 12:59:31 -0500

    permitiendo generar boletas con ruc y otros tipos para montos mayores a 700

--- app/CoreFacturalo/Requests/Api/Validation/Functions.php
+++ app/CoreFacturalo/Requests/Api/Validation/Functions.php
@@ -14,19 +14,19 @@ class Functions
 {
     public static function establishment($inputs) {
         $establishment = Establishment::where('code', $inputs['code'])->first();
-        
+
         if ($establishment) {
             return $establishment->id;
         }
-        
+
         throw new Exception("El código ingresado del establecimiento es incorrecto.");
     }
-    
+
     public static function person($inputs, $type) {
         $district_id = $inputs['district_id'];
         $province_id = ($district_id)?substr($district_id, 0 ,4):null;
         $department_id = ($district_id)?substr($district_id, 0 ,2):null;
-        
+
         $person = Person::updateOrCreate([
             'type' => $type,
             'identity_document_type_id' => $inputs['identity_document_type_id'],
@@ -42,10 +42,10 @@ class Functions
             'email' => $inputs['email'],
             'telephone' => $inputs['telephone'],
         ]);
-        
+
         return $person->id;
     }
-    
+
     public static function item($inputs) {
 
         // $establishment_id = auth()->user()->establishment->id;
@@ -67,57 +67,57 @@ class Functions
         ]);
         return $item->id;
     }
-    
+
     public static function findAffectedDocumentByExternalId($external_id) {
         $document = Document::where('external_id', $external_id)
             ->first();
-        
+
         if (!$document) throw new Exception("No se encontró el documento con código externo {$external_id}.");
-        
+
         return $document;
     }
-    
+
     public static function voidedDocuments($inputs, $type) {
         if (count($inputs['documents']) === 0) {
             throw new Exception("No se enviaron documentos para la anulación.");
         }
-        
+
         $documents = [];
         foreach ($inputs['documents'] as $row) {
             $document = Document::where('external_id', $row['external_id'])
                 ->where('date_of_issue', $inputs['date_of_reference'])
                 ->where('group_id', ($type === 'summary')?'02':'01')
                 ->first();
-            
+
             if (!$document) throw new Exception("El código externo {$row['external_id']} no fue encontrado o la fecha indica no corresponde al documento.");
-            
+
             $documents[] = [
                 'document_id' => $document->id,
                 'description' => $row['description']
             ];
         }
-        
+
         return $documents;
     }
-    
+
     public static function validateSeries($inputs) {
         $series = Series::where('number', $inputs['series'])
             ->where('document_type_id', $inputs['document_type_id'])
             ->where('establishment_id', $inputs['establishment_id'])
             ->first();
-        
+
         if (!$series) {
             throw new Exception("La serie ingresada {$inputs['series']}, es incorrecta.");
         }
     }
-    
+
     public static function DNI($inputs){
         if (($inputs['document_type_id'] == '03') && ($inputs['total']) > 700) {
             $person = Person::query()
                 ->with('identity_document_type')
                 ->find($inputs['customer_id']);
-            
-            if (!in_array($person->identity_document_type_id, ['01'])) throw new Exception("El tipo doc. identidad {$person->identity_document_type->description} del cliente no es valido.");
+
+            if (!in_array($person->identity_document_type_id, ['01','04','06','07'])) throw new Exception("El tipo doc. identidad {$person->identity_document_type->description} del cliente no es valido.");
         }
     }
 }
