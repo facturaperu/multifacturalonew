84e8fabdceab721bf5aa86f318e582f576c42af0
Brayan Mendoza Arango <bemendozaa@gmail.com>
2019-05-02 13:18:03 -0500

    rechazado

--- modules/Inventory/Providers/InventoryVoidedServiceProvider.php
+++ modules/Inventory/Providers/InventoryVoidedServiceProvider.php
@@ -24,7 +24,7 @@ class InventoryVoidedServiceProvider extends ServiceProvider
         //Revisar los tipos de documentos, ello varia el control de stock en las anulaciones.
         Document::updated(function ($document) {
             if($document['document_type_id'] == '01' || $document['document_type_id'] == '03'){
-                if($document['state_type_id'] == 11){
+                if(in_array($document['state_type_id'], [ '09', '11' ], true)){
                     $warehouse = $this->findWarehouse($document['establishment_id']);
                     foreach ($document['items'] as $detail) {
                         $this->createInventoryKardex($document, $detail['item_id'], $detail['quantity'], $warehouse->id);
