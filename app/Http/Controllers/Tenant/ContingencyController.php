efc7844f8c12230f0f000b111e858d3fbdf74292
Brayan Mendoza Arango <bemendozaa@gmail.com>
2019-05-02 14:46:52 -0500

    contingencia

--- app/Http/Controllers/Tenant/ContingencyController.php
+++ app/Http/Controllers/Tenant/ContingencyController.php
@@ -38,6 +38,7 @@ class ContingencyController extends Controller
         
         $records = Document::where($request->column, 'like', "%{$request->value}%")
                             ->whereIn('series',$series)
+                            ->whereTypeUser()
                             ->latest();
 
         return new DocumentCollection($records->paginate(config('tenant.items_per_page')));
