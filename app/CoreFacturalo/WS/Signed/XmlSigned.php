59894894903fc7e2f23c836519af4f90c06086f9
Carlos Erique Gaspar <eriquegasparcarlos>
2019-07-03 10:05:38 -0500

    actualizacion

--- app/CoreFacturalo/WS/Signed/XmlSigned.php
+++ app/CoreFacturalo/WS/Signed/XmlSigned.php
@@ -14,7 +14,7 @@ use UnexpectedValueException;
  */
 class XmlSigned
 {
-    protected $signatureId = '';
+    protected $signatureId = null;
     /* Transform */
     const ENVELOPED = 'http://www.w3.org/2000/09/xmldsig#enveloped-signature';
     const EXT_NS = 'urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2';
@@ -67,7 +67,12 @@ class XmlSigned
     public function signXml($content)
     {
         $doc = $this->getDocXml($content);
-        $this->signatureId = $doc->getElementsByTagName('Signature')->item(0)->getElementsByTagName('ID')->item(0)->nodeValue;
+        if($doc->getElementsByTagName('Signature')->length > 0) {
+            $this->signatureId = $doc->getElementsByTagName('Signature')->item(0)->getElementsByTagName('ID')->item(0)->nodeValue;
+        }
+//        else {
+//            $this->signatureId = null;
+//        }
         $this->sign($doc);
 
         return $doc->saveXML();
@@ -225,7 +230,9 @@ class XmlSigned
     protected function createXmlSecurityDSig()
     {
         $xmlSecurityDSig = new XMLSecurityDSig();
-        $xmlSecurityDSig->sigNode->setAttribute('Id', $this->signatureId);
+        if($this->signatureId) {
+            $xmlSecurityDSig->sigNode->setAttribute('Id', $this->signatureId);
+        }
         return $xmlSecurityDSig;
     }
 
