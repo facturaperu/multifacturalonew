62f9829086708a262f7e779c2123ef8bd10ca207
rauljgb <sh.raul.jgb@gmail.com>
2019-07-30 22:06:12 -0500

    cambio import document, csv a xlsx, nuevo formato

--- app/Imports/DocumentsImport.php
+++ app/Imports/DocumentsImport.php
@@ -121,7 +121,7 @@ class DocumentsImport implements ToCollection
                     ],
                     "items" => [
                         [
-                            "codigo_interno" => rtrim($row[21]),
+                            "codigo_interno" => substr($row[21],0,10),
                             "descripcion" => rtrim($row[21]),
                             "codigo_producto_sunat" => "",
                             "unidad_de_medida" => $unit_type,
@@ -170,50 +170,6 @@ class DocumentsImport implements ToCollection
                     dd($e);
                 }
 
-
-                // $description = $row[0];
-                // $item_type_id = '01';
-                // $internal_id = ($row[1])?:null;
-                // $item_code = ($row[2])?:null;
-                // $unit_type_id = $row[3];
-                // $currency_type_id = $row[4];
-                // $sale_unit_price = $row[5];
-                // $sale_affectation_igv_type_id = $row[6];
-                // $has_igv = (strtoupper($row[7]) === 'SI')?true:false;
-                // $purchase_unit_price = ($row[8])?:0;
-                // $purchase_affectation_igv_type_id = ($row[9])?:null;
-                // $stock = $row[10];
-                // $stock_min = $row[11];
-
-                // if($internal_id) {
-                //     $item = Item::where('internal_id', $internal_id)
-                //                     ->first();
-                // } else {
-                //     $item = null;
-                // }
-
-                // $establishment_id = auth()->user()->establishment->id;
-                // $warehouse = Warehouse::where('establishment_id', $establishment_id)->first();
-
-                // if(!$item) {
-                //     Item::create([
-                //         'description' => $description,
-                //         'item_type_id' => $item_type_id,
-                //         'internal_id' => $internal_id,
-                //         'item_code' => $item_code,
-                //         'unit_type_id' => $unit_type_id,
-                //         'currency_type_id' => $currency_type_id,
-                //         'sale_unit_price' => $sale_unit_price,
-                //         'sale_affectation_igv_type_id' => $sale_affectation_igv_type_id,
-                //         'has_igv' => $has_igv,
-                //         'purchase_unit_price' => $purchase_unit_price,
-                //         'purchase_affectation_igv_type_id' => $purchase_affectation_igv_type_id,
-                //         'stock' => $stock,
-                //         'stock_min' => $stock_min,
-                //         // 'warehouse_id' => $warehouse->id
-                //     ]);
-                //     $registered += 1;
-                // }
                 $registered += 1;
             }
             $this->data = compact('total', 'registered');
