b08e8d7a04ead9c05dad5c5fe0dd1a1a85dea445
PERU CPE <perucpe@gmail.com>
2019-08-10 19:49:57 -0500

    actualizacion

--- database/migrations/tenant/2019_03_01_100028_tenant_change_decimal_column_stock_to_items.php
+++ database/migrations/tenant/2019_03_01_100028_tenant_change_decimal_column_stock_to_items.php
@@ -14,7 +14,7 @@ class TenantChangeDecimalColumnStockToItems extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->decimal('stock', 12, 4)->change();
+            $table->decimal('stock', 8, 4)->change();
         });
     }
 
@@ -26,7 +26,7 @@ class TenantChangeDecimalColumnStockToItems extends Migration
     public function down()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->decimal('stock', 12, 2)->change();
+            $table->decimal('stock', 8, 2)->change();
         });
     }
 }
