8c1f6d0b3cac3810568545060c8fe3e21b48884d
Brayan Erick Mendoza Arango <b.mendoza1610@gmail.com>
2019-08-13 16:53:18 -0500

    limite meoria

--- app/Http/Controllers/Tenant/ReportController.php
+++ app/Http/Controllers/Tenant/ReportController.php
@@ -119,7 +119,7 @@ class ReportController extends Controller
             $reports = $reports->where('establishment_id', $establishment_id);
         }
 
-        set_time_limit(1200); 
+        set_time_limit(0); 
 
         $pdf = PDF::loadView('tenant.reports.report_pdf', compact("reports", "company", "establishment"));
         $filename = 'Reporte_Documentos'.date('YmdHis');
