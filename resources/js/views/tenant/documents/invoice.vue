70801054ee159f5c1c81f7f949cf840f10522fa6
b.mendoza <bemendozaa@gmail.com>
2019-12-18 18:37:27 -0500

    ajuste afectacion 15

--- resources/js/views/tenant/documents/invoice.vue
+++ resources/js/views/tenant/documents/invoice.vue
@@ -847,6 +847,20 @@
                     }
                     total_value += parseFloat(row.total_value)
                     total_plastic_bag_taxes += parseFloat(row.total_plastic_bag_taxes)
+                    
+                    //ajuste
+                    if (row.affectation_igv_type_id === '15') {
+                        let unit_value = row.unit_price / (1 + row.percentage_igv / 100)
+                        let total_value_partial = unit_value * row.quantity
+                        // row.total_value = total_value_partial
+                        row.total_taxes = (row.unit_price * row.quantity) - total_value_partial
+                        row.total_igv = (row.unit_price * row.quantity) - total_value_partial
+                        row.total_base_igv = total_value_partial
+                        total_value = 0
+                        
+                    }
+                    //ajuste
+                    console.log(row)
                 });
 
                 this.form.total_exportation = _.round(total_exportation, 2)
